25cb8e0faf932010b2c821a174f0199b621a7ebc
katjam <katjamordaunt@gmail.com>
2022-02-28 19:42:07 +0000

    Webpack-ify

--- src/Content.elm
+++ src/Content.elm
@@ -6,4 +6,12 @@ import Slides
 slidesMarkdown : List Slides.Slide
 slidesMarkdown =
     -- html, htmlFragments, md, mdFragments
-    []
+    [ Slides.md "Hello"
+    , Slides.md "hmmmm"
+    , Slides.md """
+    ```elm
+    type Hello =
+        NotPure
+    ```
+    """
+    ]
---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict'
+require('./index.html')
+
+const { Elm } = require('./Main.elm')
+
+Elm.Main.init({
+  node: document.getElementById('app')
+})
